2859229c0bcde837aa89395dac0b6fe770c1376a
M-Pidlisnyi <mishapropatriot@gmail.com>
2022-04-12 09:55:40 +0300

    added articles list on main page

--- app_blog/views.py
+++ app_blog/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
+from .models import Article
 
 # Create your views here.
 class HomePageView(TemplateView):
     def get(self, request, **kwargs):
-        return render(request, 'index.html', context=None)
+        articles = Article.objects.all()
+        return render(request, 'index.html', context={'articles': articles})
